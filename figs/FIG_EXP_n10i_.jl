233f45c59c445b36d74687453dcb4efb6ca017a9
jose.fernandez.g <jose.fernandez.g@usach.cl>
2024-08-19 19:16:26 -0300

    exp figs

---
+++ figs/FIG_EXP_n10i_.jl
@@ -0,0 +1,39 @@
+include("../src/PlotingUtils.jl")
+include("../src/NaiveCoarseness.jl")
+include("../src/GuillotineCoarseness.jl")
+include("../src/DataUtils.jl")
+
+n = 10
+i = 8
+
+S, w = readInstance(n, i)
+
+c, Π = coarseness_bf(S, w)  ## esto toma ≈1min
+
+## sol i=5 
+# c = 4
+# Π = [
+#  1
+#  2
+#  1
+#  2
+#  2
+#  1
+#  2
+#  1
+#  1
+#  1
+#     ]
+
+for k=1:size(Π)[2]
+    fig = PlotRBKpointsΠhulls(
+                S, 
+                w, 
+                Π[:,k], 
+                fig_size = (400,400), 
+                marker_size = 3
+            )
+
+    out_path = "figs/temps/FIG_EXP_n$(n)_i$(i)_k$(k).pdf"
+    savefig(fig, out_path)
+end
